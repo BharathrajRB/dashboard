6d0f96df620e0857e73e1b6d0fde3b0fd140c286
Bharath <bharathrajrb20it@gmail.com>
2023-09-16 20:34:59 +0530

    latest

--- Dashboard.js
+++ Dashboard.js
@@ -1,8 +1,12 @@
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main');
-
+var userMenu = document.querySelector('.usermenu');
 menuIcon.addEventListener('click', () => {
   sidebar.classList.toggle('sidebar-closed');
   mainContent.classList.toggle('main-content');
 });
+
+function toggleUserMenu() {
+  userMenu.classList.toggle('active');
+}
