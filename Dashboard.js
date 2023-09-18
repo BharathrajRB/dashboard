e2df57be8f8476fbb0e943794f4d4b27deeb21b7
Bharath <bharathrajrb20it@gmail.com>
2023-09-18 11:39:49 +0530

    final-commit

--- Dashboard.js
+++ Dashboard.js
@@ -2,11 +2,24 @@ const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main');
 var userMenu = document.querySelector('.usermenu');
+const navmenu = document.querySelector('.menu-iconnav');
 menuIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar-closed');
+  mainContent.classList.toggle('main-content');
+});
+navmenu.addEventListener('click', () => {
   sidebar.classList.toggle('sidebar-closed');
   mainContent.classList.toggle('main-content');
 });
 
 function toggleUserMenu() {
   userMenu.classList.toggle('active');
+}
+function toggleUserMenu1() {
+  var userMenu = document.getElementById("userMenu1");
+  if (userMenu.style.display === "block") {
+    userMenu.style.display = "none";
+  } else {
+    userMenu.style.display = "block";
+  }
 }
